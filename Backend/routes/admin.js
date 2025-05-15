63adfb959646e45d00f447c3048dcc1be7fd8c04
unknown <fariha.abd7@gmail.com>
2025-05-15 23:45:00 +0600

    fixed bcrypt

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const auth = require('../middleware/auth');
 const Subscription = require('../models/Subscription');
 const jwt = require('jsonwebtoken'); // Add this line
